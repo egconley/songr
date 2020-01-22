d21654f9abbedb33a923130649d8e22bf9920995
egconley <31393715+egconley@users.noreply.github.com>
2020-01-22 12:07:23 -0800

    added form to /Album, adds album to database

--- src/main/java/com/egconley/songr/Album.java
+++ src/main/java/com/egconley/songr/Album.java
@@ -1,6 +1,16 @@
 package com.egconley.songr;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
     String title;
     String artist;
     int songCount;
@@ -25,6 +35,14 @@ public class Album {
 
     public String getimageUrl() {
         return imageUrl;
+    }
+
+    public Album() {
+        this.title = title;
+        this.artist = artist;
+        this.songCount = songCount;
+        this.length = length;
+        this.imageUrl = imageUrl;
     }
 
     public Album(String title, String artist, int songCount, double length, String imageUrl) {

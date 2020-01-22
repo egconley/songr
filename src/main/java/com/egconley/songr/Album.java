c4b9e919fe3ce6f8d873028df26349156057a9af
egconley <31393715+egconley@users.noreply.github.com>
2020-01-22 11:14:59 -0800

    connected to database, fixed urls on album images

--- src/main/java/com/egconley/songr/Album.java
+++ src/main/java/com/egconley/songr/Album.java
@@ -11,6 +11,22 @@ public class Album {
         return title;
     }
 
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getSongCount() {
+        return songCount;
+    }
+
+    public double getlength() {
+        return length;
+    }
+
+    public String getimageUrl() {
+        return imageUrl;
+    }
+
     public Album(String title, String artist, int songCount, double length, String imageUrl) {
         this.title = title;
         this.artist = artist;
--- src/main/java/com/egconley/songr/SongrController.java
+++ src/main/java/com/egconley/songr/SongrController.java
@@ -33,9 +33,9 @@ public class SongrController {
     public String getAlbums(Model m) {
 
         Album[] albums = new Album[] {
-            new Album("Revolver", "The Beatles", 10, 60, "example.jpg"),
-            new Album("Please, please me", "The Beatles", 10, 60, "example.jpg"),
-            new Album("The White Album", "The Beatles", 20, 120, "example.jpg"),
+            new Album("Revolver", "The Beatles", 10, 60, "https://dvfnvgxhycwzf.cloudfront.net/media/SharedImage/image450/.f11pRD9T/SharedImage-18245.jpg?t=1c473f4d0f94ab09549b"),
+            new Album("Please, please me", "The Beatles", 10, 60, "https://d2s36jztkuk7aw.cloudfront.net/sites/default/files/styles/tile_2_column_x2/public/tile/image/PleasePleaseMe_1.jpg?itok=dBEFCozM&timestamp=1373996074"),
+            new Album("The White Album", "The Beatles", 20, 120, "https://d2s36jztkuk7aw.cloudfront.net/sites/default/files/styles/tile_2_column_x2/public/tile/image/original_442.jpg?itok=AeZJfLPf&timestamp=1354886431"),
         };
         m.addAttribute("albums", albums);
         return "album";
